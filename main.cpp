fe499058cb8d97867a8b1609453dd7fda5dd6a3f
Jesus Bernat <contact@asedem.com>
2024-08-02 10:39:10 +0200

    Compilada y usada librería dinámica .DLL

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-#include "test.h"
+#include <windows.h>
 using namespace std;
 
+typedef void (*rpFunc)();
+typedef void (*preguntaFunc)(int x);
+typedef void (*checkFunc)(int x, string r);
+
 int main() {
+  HMODULE hModule = LoadLibraryA("test.dll");
+  rpFunc rellenaPreguntas = (rpFunc)GetProcAddress(hModule, "rellenaPregutas");
+  preguntaFunc pregunta = (preguntaFunc)GetProcAddress(hModule, "pregunta");
+  preguntaFunc getEnunciado = (preguntaFunc)GetProcAddress(hModule, "getEnunciado");
+  preguntaFunc getRespuestas = (preguntaFunc)GetProcAddress(hModule, "getRespuestas");
+  checkFunc checkRespuesta = (checkFunc)GetProcAddress(hModule, "checkRespuesta");
 
   rellenaPreguntas();
 
--- test.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-using namespace std;
-
-void rellenaPreguntas();
-void getEnunciado(int n);
-void getRespuestas(int n);
-void checkRespuesta(int n, string r);
-
-void pregunta(int numPregunta);
