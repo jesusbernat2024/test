b9b9db1193c33ac2fcdca4910faf03259c7b1f58
Jesus Bernat <contact@asedem.com>
2024-08-05 08:44:20 +0200

    Corrección error nombre función

--- main.cpp
+++ main.cpp
@@ -4,15 +4,11 @@ using namespace std;
 
 typedef void (*rpFunc)();
 typedef void (*preguntaFunc)(int x);
-typedef void (*checkFunc)(int x, string r);
 
 int main() {
   HMODULE hModule = LoadLibraryA("test.dll");
-  rpFunc rellenaPreguntas = (rpFunc)GetProcAddress(hModule, "rellenaPregutas");
+  rpFunc rellenaPreguntas = (rpFunc)GetProcAddress(hModule, "rellenaPreguntas");
   preguntaFunc pregunta = (preguntaFunc)GetProcAddress(hModule, "pregunta");
-  //preguntaFunc getEnunciado = (preguntaFunc)GetProcAddress(hModule, "getEnunciado");
-  //preguntaFunc getRespuestas = (preguntaFunc)GetProcAddress(hModule, "getRespuestas");
-  //checkFunc checkRespuesta = (checkFunc)GetProcAddress(hModule, "checkRespuesta");
 
   rellenaPreguntas();
 
